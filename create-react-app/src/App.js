4923b363c18aecdd3056ba84b6486b7ce64b6e51
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2022-11-17 09:10:11 +0545

    html tags

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div>App</div>
+      <p>Paragraph here</p>
+
+      <header>Hello</header>
+      <footer>Footer</footer>
+      <section></section>
+      <nav></nav>
+      <main></main>
+      <article></article>
+      <aside></aside>
+      <figure></figure>
+
+      <img />
+      <table></table>
+      <button></button>
+      <form></form>
+    </>
+  );
 }
 
 export default App;
