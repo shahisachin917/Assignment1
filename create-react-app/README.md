008502819f2c2386c421935141af132cc6659632
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2022-11-17 08:44:08 +0545

    app refactored

--- create-react-app/README.md
+++ create-react-app/README.md
@@ -1,6 +1,6 @@
 # Today's Goal
 
-1. Installing React environment using Create React App
-2. Creating First Component with React
-3. Writing HTML (JSX) and CSS with React
-4. Semantic HTML in React
+## HTML and CSS with React
+
+    1. Semantic HTML in React
+    2. Basic CSS with React
--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,30 +1,7 @@
 import React from "react";
-import "./App.css";
-import Contact from "./Front/Contact";
-import Home from "./Front/Home";
-
-// Function in JavaScript -> Component in React
-// HTML without React-> JSX with React
 
 function App() {
-  return (
-    <>
-      <Home />
-      {/* Self Closing Tag */}
-      <h1>React App</h1>
-      <div>App</div>
-    </>
-  );
+  return <div>App</div>;
 }
 
 export default App;
-
-// div
-// h1
-// header
-// main
-// section
-// footer
-// aside
-// ul
-// li
