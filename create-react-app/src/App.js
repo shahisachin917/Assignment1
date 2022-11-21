42ddf8be7847cde5b147b75d8da9d200045af0a6
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2022-11-21 09:11:47 +0545

    const and let

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -5,12 +5,17 @@ function App() {
   const myCountry = "Nepal";
   const myName = "Gorakh";
   const favNumber = 7;
+  // const age = 30;
+  let age = 30;
+
+  age = 31;
 
   return (
     <>
       <h1>My Country Name: {myCountry}</h1>
       <h2>My Name: {myName}</h2>
       <h2>My Favourite Number: {favNumber}</h2>
+      <h2>My Age: {age}</h2>
     </>
   );
 }
