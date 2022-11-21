3b5bb4b39d7f3ce5ccfddd152e82a65b97e96eef
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2022-11-21 07:29:56 +0545

    css box model start

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -3,17 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      App
-      <p>Hello</p>
-      {/* block level html elements */}
-      <span>I am span</span>
-      {/* inline elements */}
-      <div className="innerdiv">I am a first div inside another div</div>
-      <div className="innerdiv">I am a second div inside another div</div>
-      <div className="innerdiv">I am a third div inside another div</div>
-      <div id="fourthdiv">I am a fourth div inside another div</div>
-    </div>
+    <>
+      <div>CSS Box Model</div>
+    </>
   );
 }
 
