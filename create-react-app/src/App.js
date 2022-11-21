8f2830ca2a2bb78c87a1b9a87571cb80022c0323
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2022-11-21 08:38:28 +0545

    css box model final

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>CSS Box Model</div>
+      <div className="box">CSS Box Model</div>
+      <span>I am inline element</span>
+      <div className="square"></div>
     </>
   );
 }
