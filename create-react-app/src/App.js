af895c25c81286bcbaa9e3bb7bdbae5d446fbb08
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2022-11-22 08:59:09 +0545

    function and conditionals in javascript

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,38 +1,20 @@
 import React from "react";
 import "./App.css";
 
+import { add, output } from "./utils/utility";
+
 const App = function () {
-  // Function Declaration
-  // function add(a, b) {
-  //   return a + b;
-  // }
-
-  // Function Expression
-  // const add = function (a, b) {
-  //   return a + b;
-  // };
-
-  // Arrow Function
-  // const add = (a, b) => {
-  //   return a + b;
-  // };
-  // const add = (a, b) => {
-  //   console.log(a);
-  //   console.log(b);
-  //   return a + b;
-  // };
-
-  const add = (a, b) => a + b;
-
   const sum = add(5, 7);
-  const output = add(50, 70);
+  const result = output(5);
 
   return (
     <>
-      <h1>First Sum: {sum + 20}</h1>
-      <h1>Second Sum: {output}</h1>
+      <h1>Result coming from sum function: {sum + 20}</h1>
+      <h1>Result coming from output function: {result}</h1>
     </>
   );
 };
+
+// Seperation of Concern
 
 export default App;
---
+++ create-react-app/src/utils/utility.js
@@ -0,0 +1,9 @@
+export const add = (a, b) => a + b;
+
+export const output = (x) => {
+  if (x > 10) {
+    return "Your number is greater than 10";
+  } else {
+    return "Your number is less than 10";
+  }
+};
