57d97337fef6d2ab345d168828d0bc12e49f9f21
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2022-11-22 07:29:30 +0545

    css media query

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -10,13 +10,15 @@ function App() {
 
   age = 32;
 
-
   return (
     <>
       <h1>My Country Name: {myCountry}</h1>
       <h2>My Name: {myName}</h2>
       <h2>My Favourite Number: {favNumber}</h2>
       <h2>My Age: {age}</h2>
+
+      <div className="large-screen">I am only visible in large screen</div>
+      <div className="small-screen">I am only visible in small screen</div>
     </>
   );
 }
